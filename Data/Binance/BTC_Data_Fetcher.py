ef15e7acdd162184889ab70da1c112474dfed7fa
MY-Raza <yasirrazaattari2002@gmail.com>
2026-01-13 15:15:10 +0500

    Fourth

--- Data/Binance/BTC_Data_Fetcher.py
+++ Data/Binance/BTC_Data_Fetcher.py
@@ -59,3 +59,53 @@ def fetch_1m_data(symbol, start_ts, end_ts, output_path):
     df.to_csv(output_path, index=False)
 
     print(f"Saved: {output_path}")
+
+# Function for Fetching Future Data
+
+def fetch_1m_futures_data(symbol, start_ts, end_ts, output_path):
+    interval = Client.KLINE_INTERVAL_1MINUTE
+    all_klines = []
+
+    while start_ts < end_ts:
+        print(f"Fetching FUTURES {symbol} from {datetime.utcfromtimestamp(start_ts/1000)}")
+
+        klines = client.futures_klines(
+            symbol=symbol,
+            interval=interval,
+            startTime=start_ts,
+            endTime=end_ts,
+            limit=1000
+        )
+
+        if not klines:
+            break
+
+        all_klines.extend(klines)
+        start_ts = klines[-1][0] + 1
+        time.sleep(0.5)
+
+    raw_columns = [
+        "timestamp", "open", "high", "low", "close", "volume",
+        "close_time", "quote_asset_volume",
+        "number_of_trades",
+        "taker_buy_base_volume",
+        "taker_buy_quote_volume",
+        "ignore"
+    ]
+
+    df = pd.DataFrame(all_klines, columns=raw_columns)
+
+    df = df[["timestamp", "open", "high", "low", "close", "volume"]]
+
+    df["timestamp"] = df["timestamp"].astype("int64")
+    df[["open", "high", "low", "close", "volume"]] = df[
+        ["open", "high", "low", "close", "volume"]
+    ].astype(float)
+
+    # Safety: remove duplicates & sort
+    df = df.drop_duplicates(subset="timestamp").sort_values("timestamp")
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_csv(output_path, index=False)
+
+    print(f"FUTURES saved: {output_path}")
--- Data/Binance/main.py
+++ Data/Binance/main.py
@@ -1,6 +1,6 @@
 import yaml
 from datetime import datetime, timedelta
-from BTC_Data_Fetcher import fetch_1m_data
+from BTC_Data_Fetcher import fetch_1m_data, fetch_1m_futures_data 
 
 with open("config.yml", "r") as f:
     config = yaml.safe_load(f)
@@ -18,10 +18,19 @@ else:
 
 # Fetch data
 for sym in symbols:
-    symbol = sym.upper() + "USDT"
-    output_file = f"data/{symbol}_1m.csv"
+    #symbol = sym.upper() + "USDT"
+    #output_file = f"data/{symbol}_1m.csv"
 
-    fetch_1m_data(
+    #fetch_1m_data(
+    #    symbol=symbol,
+    #    start_ts=start_ts,
+    #    end_ts=end_ts,
+    #    output_path=output_file
+    #)
+    symbol = sym.upper() + "USDT"
+    output_file = f"data/futures/{symbol}_1m_last_7_days.csv"
+
+    fetch_1m_futures_data(
         symbol=symbol,
         start_ts=start_ts,
         end_ts=end_ts,
