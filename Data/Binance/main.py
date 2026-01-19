fa5a236754b4216e5af9f86362fe1f3253948480
MY-Raza <yasirrazaattari2002@gmail.com>
2026-01-19 17:52:30 +0500

    17th19

--- Data/Binance/main.py
+++ Data/Binance/main.py
@@ -1,4 +1,4 @@
-from TradeX.utils.db.utils import save_df_to_db
+from TradeX.utils.db.utils import save_df_to_db,get_last_date
 from TradeX.utils.common.logs import get_logger
 from binance_fetcher import BinanceFuturesFetcher
 from TradeX.utils.data.data_cleaner import clean_df
@@ -44,6 +44,17 @@ end_date = config["end_date"]
 # -------------------------------------------------
 for symbol in symbols:
     logger.info(f"Starting data pipeline for symbol: {symbol}")
+    table_name = symbol.lower()
+    last_stored_date = get_last_date(table_name=table_name, schema=SCHEMA, time_column="timestamp")
+
+    if last_stored_date:
+        # Start from the last timestamp in the database
+        start_date = last_stored_date.strftime("%Y-%m-%d")
+        logger.info(f"Found existing data for {symbol}. Setting start_date={start_date}")
+    else:
+        # Use default start date from config
+        start_date = start_date
+        logger.info(f"No existing data found for {symbol}. Using start_date={start_date}")
 
     # Initialize Binance fetcher
     fetcher = BinanceFuturesFetcher(
