155fbabe61b0005fce4cd25a45a36475af6afb6f
MY-Raza <yasirrazaattari2002@gmail.com>
2026-01-22 11:25:26 +0500

    4th22

--- Data/kraken/sample.py
+++ Data/kraken/sample.py
@@ -1,5 +1,7 @@
 from TradeX.data.kraken.kraken_fetcher import KrakenFuturesFetcher
+from TradeX.utils.data.data_cleaner import clean_df
 
 fetcher = KrakenFuturesFetcher(symbol="PF_XBTUSD", interval="1m")
-df = fetcher.fetch(start_date="2024-01-01", end_date="now")
+df = fetcher.fetch(start_date="2026-01-15", end_date="now")
+df = clean_df(df)
 fetcher.save_to_csv(df, "kraken_PF_XBTUSD_ohlcv.csv")
