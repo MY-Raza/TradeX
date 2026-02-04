20dd2b50bd9d529f56586a181d8fcdb408f9bb1b
MY-Raza <yasirrazaattari2002@gmail.com>
2026-02-04 17:49:16 +0500

    11th4

--- strategy_generator/main.py
+++ strategy_generator/main.py
@@ -24,22 +24,88 @@ RUNS_PER_TIMEFRAME = 50
 # Indicators
 # ============================
 ALL_INDICATORS = (
+    # -------------------------
+    # Overlap Studies
+    # -------------------------
     "BBANDS", "DEMA", "EMA", "HT_TRENDLINE", "KAMA",
     "MA", "MAMA", "MIDPOINT", "MIDPRICE", "SAR",
     "SAREXT", "SMA", "T3", "TEMA", "TRIMA", "WMA",
+    # -------------------------
+    # Momentum Indicators
+    # -------------------------
     "ADX", "ADXR", "APO", "AROON", "AROONOSC",
-    "BOP", "CCI", "CMO", "DX", "MACD", "MACDEXT",
-    "MACDFIX", "MFI", "MINUS_DI", "MINUS_DM",
-    "MOM", "PLUS_DI", "PLUS_DM", "PPO", "ROC",
-    "ROCP", "ROCR", "ROCR100", "RSI", "STOCH",
-    "STOCHF", "STOCHRSI", "TRIX", "ULTOSC",
-    "WILLR", "AD", "ADOSC", "OBV", "ATR", "NATR",
-    "TRANGE", "AVGPRICE", "MEDPRICE", "TYPPRICE",
-    "WCLPRICE", "HT_DCPERIOD", "HT_DCPHASE",
-    "HT_PHASOR", "HT_SINE", "HT_TRENDMODE",
-    "LINEARREG", "LINEARREG_ANGLE",
+    "BOP", "CCI", "CMO", "DX", "MACD",
+    "MACDEXT", "MACDFIX", "MFI", "MINUS_DI",
+    "MINUS_DM", "MOM", "PLUS_DI", "PLUS_DM",
+    "PPO", "ROC", "ROCP", "ROCR", "ROCR100",
+    "RSI", "STOCH", "STOCHF", "STOCHRSI",
+    "TRIX", "ULTOSC", "WILLR",
+    # -------------------------
+    # Volume Indicators
+    # -------------------------
+    "AD", "ADOSC", "OBV",
+    # -------------------------
+    # Volatility Indicators
+    # -------------------------
+     "ATR", "NATR", "TRANGE",
+    # -------------------------
+    # Price Transform Indicators
+    # -------------------------
+    "AVGPRICE", "MEDPRICE", "TYPPRICE", "WCLPRICE",
+    # -------------------------
+    # Cycle Indicators
+    # -------------------------
+    "HT_DCPERIOD", "HT_DCPHASE", "HT_PHASOR",
+    "HT_SINE", "HT_TRENDMODE",
+    # -------------------------
+    # Statistic Indicators
+    # -------------------------
+    "BETA", "CORREL", "LINEARREG", "LINEARREG_ANGLE",
     "LINEARREG_INTERCEPT", "LINEARREG_SLOPE",
-    "STDDEV", "TSF", "VAR"
+    "STDDEV", "TSF", "VAR",
+    # -------------------------
+    # Math Transform Indicators
+    # -------------------------
+    "ACOS", "ASIN", "ATAN", "CEIL", "COS", "COSH",
+    "EXP", "FLOOR", "LN", "LOG10", "SIN", "SINH",
+    "SQRT", "TAN", "TANH",
+    # -------------------------
+    # Math Operators
+    # -------------------------
+    "ADD", "DIV", "MAX", "MAXINDEX", "MIN",
+    "MININDEX", "MULT", "SUB", "SUM",
+    # ---------------------------
+    # CANDLESTICK PATTERN 
+    # --------------------------
+    "CDL2CROWS", "CDL3BLACKCROWS", "CDL3INSIDE",
+    "CDL3LINESTRIKE", "CDL3OUTSIDE", "CDL3STARSINSOUTH",
+    "CDL3WHITESOLDIERS", "CDLABANDONEDBABY",
+    "CDLADVANCEBLOCK", "CDLBELTHOLD", "CDLBREAKAWAY",
+    "CDLCLOSINGMARUBOZU", "CDLCONCEALBABYSWALL",
+    "CDLCOUNTERATTACK", "CDLDARKCLOUDCOVER",
+    "CDLDOJI", "CDLDOJISTAR", "CDLDRAGONFLYDOJI",
+    "CDLENGULFING", "CDLEVENINGDOJISTAR",
+    "CDLEVENINGSTAR", "CDLGAPSIDESIDEWHITE",
+    "CDLGRAVESTONEDOJI", "CDLHAMMER",
+    "CDLHANGINGMAN", "CDLHARAMI",
+    "CDLHARAMICROSS", "CDLHIGHWAVE",
+    "CDLHIKKAKE", "CDLHIKKAKEMOD",
+    "CDLHOMINGPIGEON", "CDLIDENTICAL3CROWS",
+    "CDLINNECK", "CDLINVERTEDHAMMER",
+    "CDLKICKING", "CDLKICKINGBYLENGTH",
+    "CDLLADDERBOTTOM", "CDLLONGLEGGEDDOJI",
+    "CDLLONGLINE", "CDLMARUBOZU",
+    "CDLMATCHINGLOW", "CDLMATHOLD",
+    "CDLMORNINGDOJISTAR", "CDLMORNINGSTAR",
+    "CDLONNECK", "CDLPIERCING",
+    "CDLRICKSHAWMAN", "CDLRISEFALL3METHODS",
+    "CDLSEPARATINGLINES", "CDLSHOOTINGSTAR",
+    "CDLSHORTLINE", "CDLSPINNINGTOP",
+    "CDLSTALLEDPATTERN", "CDLSTICKSANDWICH",
+    "CDLTAKURI", "CDLTASUKIGAP",
+    "CDLTHRUSTING", "CDLTRISTAR",
+    "CDLUNIQUE3RIVER", "CDLUPSIDEGAP2CROWS",
+    "CDLXSIDEGAP3METHODS"
 )
 
 # ============================
